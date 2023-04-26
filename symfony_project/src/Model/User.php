c7e79b9e76393d50885cbcc774005d45cec2eab9
marija <mmilosevic1411@gmail.com>
2023-04-26 23:46:20 +0200

    database .env property set and entity created

---
+++ symfony_project/src/Model/User.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Model;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface, \Serializable
+{
+    private $id;
+
+    private $firstName;
+    private $lastName;
+    private $email;
+    private $username;
+    private $password;
+    private $address;
+    private $postNumber;
+    private $city;
+    private $country;
+
+    public function serialize()
+    {
+        // TODO: Implement serialize() method.
+    }
+
+    public function unserialize(string $data)
+    {
+        // TODO: Implement unserialize() method.
+    }
+
+    public function getRoles(): array
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        // TODO: Implement getUserIdentifier() method.
+    }
+
+    public function __serialize(): array
+    {
+        // TODO: Implement __serialize() method.
+    }
+
+    public function __unserialize(array $data): void
+    {
+        // TODO: Implement __unserialize() method.
+    }
+}
