d21a4d9489c6792b05a063f6514e10620b56b79e
marija <mmilosevic1411@gmail.com>
2023-05-02 23:04:51 +0200

    Registration, but not connecting to database

--- symfony_project/src/Controller/RegistrationController.php
+++ symfony_project/src/Controller/RegistrationController.php
@@ -9,28 +9,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+//use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/register")
+     * @Route("/registration")
      * @param Request $request
      * @param $encoder
      * @return Response
      */
-    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    public function register(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
-            $em->persist($user);
-            $em->flush();
-
+//            $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
             return $this->redirectToRoute('app_homepage_homepage');
         }
 
